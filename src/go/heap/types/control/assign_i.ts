45a8e182eca348274c4536d869d146c1fd68a59c
benson1029 <yeungmantsung@gmail.com>
2024-03-15 17:52:26 +0800

    Clean up code

--- src/go/heap/types/control/assign_i.ts
+++ src/go/heap/types/control/assign_i.ts
@@ -22,8 +22,6 @@ class ControlAssignI extends HeapObject {
   public static allocate(heap: Heap, name: ComplexString): number {
     const address = heap.allocate_object(TAG_CONTROL_assign_i, 1, 1);
     heap.set_child(address, 0, name.address);
-    heap.set_cannnot_be_freed(address, false);
-
     return address;
   }
 }
