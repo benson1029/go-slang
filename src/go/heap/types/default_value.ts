1f19940759a0317f50114e054451acb9377ea5d8
Rama Aryasuta Pangestu <rama.a.pangestu@gmail.com>
2024-03-21 20:54:56 +0800

    Fix default_value implementation

--- src/go/heap/types/default_value.ts
+++ src/go/heap/types/default_value.ts
@@ -5,29 +5,21 @@ import { PrimitiveInt32 } from "./primitive/int32";
 import { PrimitiveRune } from "./primitive/rune";
 
 function default_value(heap: Heap, type: string, value: any) {
+  if (value != null) {
+    return value;
+  }
+
   switch (type) {
     case "int32":
-      return PrimitiveInt32.allocate(
-        heap,
-        value === null || value === undefined ? 0 : value
-      );
+      return PrimitiveInt32.allocate(heap, 0);
     case "float32":
-      return PrimitiveFloat32.allocate(
-        heap,
-        value === null || value === undefined ? 0 : value
-      );
+      return PrimitiveFloat32.allocate(heap, 0);
     case "rune":
-      return PrimitiveRune.allocate(
-        heap,
-        value === null || value === undefined ? 0 : value
-      );
+      return PrimitiveRune.allocate(heap, 0);
     case "bool":
-      return PrimitiveBool.allocate(
-        heap,
-        value === null || value === undefined ? false : value
-      );
+      return PrimitiveBool.allocate(heap, false);
     default:
-      return value;
+      throw new Error(`Unknown type: ${type}`);
   }
 }
 
