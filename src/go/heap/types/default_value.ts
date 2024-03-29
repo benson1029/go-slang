58db69975c3672305cdc1ef4dd950568c7913d8b
benson1029 <yeungmantsung@gmail.com>
2024-03-29 15:04:19 +0800

    Add string

--- src/go/heap/types/default_value.ts
+++ src/go/heap/types/default_value.ts
@@ -1,5 +1,6 @@
 import { Heap } from "../heap";
 import {
+  TAGSTRING_COMPLEX_string,
   TAGSTRING_PRIMITIVE_bool,
   TAGSTRING_PRIMITIVE_float32,
   TAGSTRING_PRIMITIVE_int32,
@@ -20,6 +21,8 @@ function default_value(heap: Heap, type: string, value: any) {
       return { tag: "literal", type: type, value: 0 };
     case TAGSTRING_PRIMITIVE_bool:
       return { tag: "literal", type: type, value: false };
+    case TAGSTRING_COMPLEX_string:
+      return { tag: "literal", type: type, value: "" };
     default:
       throw new Error(`Unknown type: ${type}`);
   }
